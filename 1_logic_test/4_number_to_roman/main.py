2c66d36f12130df49a7cff1ee48b4b8500bf98cb
artitaya <artitaya2466@gmail.com>
2026-05-15 14:58:33 +0700

    number to roman

---
+++ 1_logic_test/4_number_to_roman/main.py
@@ -0,0 +1,39 @@
+"""
+เขียบนโปรแกรมแปลงตัวเลยเป็นตัวเลข roman
+
+[Input]
+number: list of numbers
+
+[Output]
+roman_text: roman number
+
+[Example 1]
+input = 101
+output = CI
+
+[Example 2]
+input = -1
+output = number can not less than 0
+"""
+
+
+class Solution:
+
+    def number_to_roman(self, number: int) -> str:
+        if number < 0:
+            return "number can not less than 0"
+        if number == 0:
+            return ""
+
+        roman = [ (1000, "M"),(900, "CM"),(500, "D"),(400, "CD"),(100, "C"),(90, "XC"),(50, "L"),(40, "XL"),(10, "X"),(9, "IX"),(5, "V"),(4, "IV"),(1, "I")]    
+        result = ""
+
+        for value , symbol in roman:
+            while number >= value:
+                result += symbol
+                number -= value
+
+        return result        
+
+sol = Solution()
+print(sol.number_to_roman(101))
